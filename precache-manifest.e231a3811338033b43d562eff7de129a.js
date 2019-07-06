5db6f6244cedbc1141e8871597b130a1ac36b8f2
H Cui <hcui8000@gmail.com>
2019-07-06 12:01:57 -0700

    Updates

---
+++ precache-manifest.e231a3811338033b43d562eff7de129a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60eb13622a595c651e1fd2629d76a774",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "b2e8e3f7467e401abe9d",
+    "url": "/monsters-rolodex/static/css/main.59ed0e04.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "b2e8e3f7467e401abe9d",
+    "url": "/monsters-rolodex/static/js/main.978a4dd9.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
